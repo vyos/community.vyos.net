cace31b794a5a81ad3b3aa1bebca6afa26e071a9
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-06-20 14:17:09 +0300

    added one month date of expiration for user's cookie consent
    (cherry picked from commit 7863f97ba712fe6e7801eee8c29c955000b6560a)

--- site/js/google-analytics-message.js
+++ site/js/google-analytics-message.js
@@ -1,48 +1,57 @@
-// function to toggle Google Analytics message
-const toggleGoogleAnalyticsMessage = action => {
-  const message = document.getElementById('google-analytics-message')
-  action === 'show' && message.classList.add('open')
-  action === 'hide' && message.classList.remove('open')
-}
+document.addEventListener('DOMContentLoaded', function (event) {
 
-// check if cookies with user choice exist
-const isGoogleAnalyticsAllowedValueExist =
-  document.cookie.split(';')
-    .find(row => row.includes('isGoogleAnalyticsAllowed'))
-
-// if the user has already made a choice
-if (isGoogleAnalyticsAllowedValueExist) {
-
-  const isGoogleAnalyticsAllowedValue = isGoogleAnalyticsAllowedValueExist.split('=')[1]
-
-  // and if the user chose "Accept" - connect Google Analytics
-  if (isGoogleAnalyticsAllowedValue === 'yes') {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-J3WHFQG00P');
+  function determineDataOfExpiration() {
+    const date = new Date();
+    date.setMonth(date.getMonth() + 1);  // Add one month to the current date
+    const expiresString = "expires=" + date.toUTCString()
+    return expiresString
+  }
+	// function to toggle Google Analytics message
+  const toggleGoogleAnalyticsMessage = action => {
+    const message = document.getElementById('google-analytics-message')
+    action === 'show' && message.classList.add('open')
+    action === 'hide' && message.classList.remove('open')
   }
 
-} else {
+  // check if cookies with user choice exist
+  const isGoogleAnalyticsAllowedValueExist =
+    document.cookie.split(';')
+      .find(row => row.includes('isGoogleAnalyticsAllowed'))
 
-  // display a message with a question
-  toggleGoogleAnalyticsMessage('show')
+  // if the user has already made a choice
+  if (isGoogleAnalyticsAllowedValueExist) {
 
-  const googleAnalyticsMessageAccept = document.querySelector('#google-analytics-accept')
-  const googleAnalyticsMessageDecline = document.querySelectorAll('#google-analytics-decline')
+    const isGoogleAnalyticsAllowedValue = isGoogleAnalyticsAllowedValueExist.split('=')[1]
 
-  // register a click on the "Accept" button
-  googleAnalyticsMessageAccept.addEventListener('click', function () {
-    document.cookie = 'isGoogleAnalyticsAllowed=yes'
-    toggleGoogleAnalyticsMessage('hide')
-  })
+    // and if the user chose "Accept" - connect Google Analytics
+    if (isGoogleAnalyticsAllowedValue === 'yes') {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-J3WHFQG00P');
+    }
 
-  // register a click on the "Decline" button
-  googleAnalyticsMessageDecline.forEach(el => {
-    el.addEventListener('click', function () {
-      document.cookie = 'isGoogleAnalyticsAllowed=no'
+  } else {
+
+    // display a message with a question
+    toggleGoogleAnalyticsMessage('show')
+
+    const googleAnalyticsMessageAccept = document.querySelector('#google-analytics-accept')
+    const googleAnalyticsMessageDecline = document.querySelectorAll('#google-analytics-decline')
+
+    // register a click on the "Accept" button
+    googleAnalyticsMessageAccept.addEventListener('click', function () {
+      document.cookie = `isGoogleAnalyticsAllowed=yes;${determineDataOfExpiration()};path=/`
       toggleGoogleAnalyticsMessage('hide')
     })
-  })
 
-}
+    // register a click on the "Decline" button
+    googleAnalyticsMessageDecline.forEach(el => {
+      el.addEventListener('click', function () {
+        document.cookie = `isGoogleAnalyticsAllowed=no;${determineDataOfExpiration()};path=/`
+        toggleGoogleAnalyticsMessage('hide')
+      })
+    })
+
+  }
+})
